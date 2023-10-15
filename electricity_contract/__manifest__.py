5a69da07b6660c3e054e5f63c91735d5f7035ce4
SJ <sacha.jumpertz@gmail.com>
2023-10-15 14:50:22 +0200

    [ADD][V1.0]electricity_contract: add menu
    create the module with the 2 mandatory field
    add it in main menu
    create form view

--- electricity_contract/__manifest__.py
+++ electricity_contract/__manifest__.py
@@ -4,9 +4,15 @@
     'name': 'Electricity Contract',
     'version': '1.0',
     'author': "Minijump",
+    'application': True,
+    'category': 'Uncategorized',
     'summary': 'Create Electricity contract',
     'description': "Create Electricity contract",
     'depends': [
         'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/electricity_contract_views.xml',  
     ],
 }
--- electricity_contract/models/Electricity_contract.py
+++ electricity_contract/models/Electricity_contract.py
@@ -5,3 +5,10 @@ class ElectricityContract(models.Model):
     _description = "Represent electricity contracts"
 
     name = fields.Char(required=True)
+    company_id = fields.Many2one('res.company', default=lambda self: self.env.company)
+    currency_id = fields.Many2one('res.currency', related='company_id.currency_id')
+    price = fields.Monetary(required=True, string="Price of a kWh")
+
+    _sql_constraints = [
+        ('unique_name', 'UNIQUE(name)', 'The name must be unique.')
+    ]
