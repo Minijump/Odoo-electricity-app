5903c4f500465b6491b835193d6b2bf6cf5cf97b
SJ <sacha.jumpertz@gmail.com>
2023-11-01 13:58:17 +0100

    [ADD][V2]improve UX
    fields was changed when saved, the need is that computed field must be
    changed when the fields they depends are changed.
    Note that the computation of electricity cost is still not dynamic,
    And I still don't know why et ca me casse les couilles

--- electricity_contract/models/product_template.py
+++ electricity_contract/models/product_template.py
@@ -21,14 +21,18 @@ class ProductTemplate(models.Model):
     electricity_cost = fields.Monetary(compute="_compute_elec_cost", string="Cost of the elec. consumption")
     cost_with_elec = fields.Monetary(compute="_compute_cost_with_elec", string="Cost of product with electricity")
 
+    #still don't know why depends change standard_price
+    #@api.depends('price_elec_contract','electricity_consumption', 'convert_units')
     def _compute_elec_cost(self):
         for rec in self:
-            rec.electricity_cost = rec.price_elec_contract * rec.electricity_consumption * rec.convert_units
+            rec.electricity_cost = rec.electricity_consumption * rec.price_elec_contract * rec.convert_units
 
+    @api.depends('electricity_cost', 'standard_price')
     def _compute_cost_with_elec(self):
         for rec in self:
             rec.cost_with_elec = rec.electricity_cost + rec.standard_price 
 
+    @api.depends('electricity_uom', 'electricity_contract_id.uom')
     def _compute_conversion(self):
         conversion_factors = {
             ('wh', 'wh'): 1,
